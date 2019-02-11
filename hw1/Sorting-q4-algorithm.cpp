da67bc6ba68449a9e5ca8b15dfae6bf15cc43160
Mike Daly <daly2@stolaf.edu>
2019-02-11 01:22:54 -0600

    adding commented assembly file

---
+++ hw1/Sorting-q4-algorithm.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+#include <stdlib.h>
+#include "elapsed_time.h"
+
+int *initializeArray( int N)
+{
+    //Initialize array of length N with radom values
+    int *array = new int[N];
+    for ( int i=0; i<N;++i) array[i]=rand()%100;
+    return array;
+}
+void printArray(int *arr, int N)
+{
+    for ( int i=0; i<N; ++i) cout << " " << arr[i];
+    cout << endl;
+}
+int findx(int N)
+{
+    int x = rand()%(N-1);
+    return x;
+}
+int smallerthan(int x, int *arr, int N)
+{
+    int smaller = 0;
+    for ( int i =0; i<N; ++i)
+    {
+        if(arr[i]<arr[x])
+        {
+            smaller++;
+        }
+    }
+    return smaller;
+}
+int *copy(int *array, int N, int x, int smaller)
+{
+    int *arr2= new int[N];
+    arr2[smaller] = array[x];
+    int less=0;
+    int more=smaller+1;
+    for(int i =0; i<N; i++)
+    {
+        if(array[i]<array[x])
+        {
+            arr2[less]=array[i];
+            less++;
+        }
+        else if( array[i]>=array[x]&&x!=i)
+        {
+            arr2[more]=array[i];
+            more++;
+        }
+    }
+    //printArray(arr2, N);
+    delete [] array;
+    return arr2;
+}
+bool check(int *array, int N)
+{
+    for( int i=0; i<N-1; i++)
+    {
+        if(array[i]>array[i+1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int *sortArray(int *array, int N)
+{
+    bool sorted =false;
+    while( !sorted )
+    {
+        int x = findx(N);
+        int smaller=smallerthan(x, array, N);
+        array=copy(array, N, x, smaller);
+        sorted = check(array, N);
+    }
+    return array;
+}
+int main()
+{
+    const int N = 10;
+    const bool verbose = true;
+    int *array= initializeArray(N);
+    if ( verbose ) { cout << "input:"; printArray(array, N); }
+    start_timer();
+    array=sortArray(array, N);
+    double cycles = elapsed_time();
+    if ( verbose ) { cout<< "Sorted:"; printArray(array,N); }
+    cout << " Total cycles: " << cycles << endl;
+    delete [] array;
+}
---
+++ hw1/a_loop.s
@@ -0,0 +1,228 @@
+	.file	"a_loop.cpp"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.globl	start_time
+	.bss
+	.align 8
+	.type	start_time, @object
+	.size	start_time, 8
+start_time:
+	.zero	8
+	.section	.text._Z8read_TSCv,"axG",@progbits,_Z8read_TSCv,comdat
+	.weak	_Z8read_TSCv
+	.type	_Z8read_TSCv, @function
+_Z8read_TSCv:
+.LFB1493:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+#APP
+# 15 "elapsed_time.h" 1
+	rdtsc
+# 0 "" 2
+#NO_APP
+	salq	$32, %rdx
+	orq	%rdx, %rax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1493:
+	.size	_Z8read_TSCv, .-_Z8read_TSCv
+	.section	.text._Z11start_timerv,"axG",@progbits,_Z11start_timerv,comdat
+	.weak	_Z11start_timerv
+	.type	_Z11start_timerv, @function
+_Z11start_timerv:
+.LFB1494:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	call	_Z8read_TSCv
+	movq	%rax, start_time(%rip)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1494:
+	.size	_Z11start_timerv, .-_Z11start_timerv
+	.section	.text._Z12elapsed_timev,"axG",@progbits,_Z12elapsed_timev,comdat
+	.weak	_Z12elapsed_timev
+	.type	_Z12elapsed_timev, @function
+_Z12elapsed_timev:
+.LFB1495:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	call	_Z8read_TSCv
+	movq	%rax, -16(%rbp)
+	movq	start_time(%rip), %rax
+	movq	-16(%rbp), %rdx
+	subq	%rax, %rdx
+	movq	%rdx, %rax
+	testq	%rax, %rax
+	js	.L5
+	cvtsi2sdq	%rax, %xmm0
+	jmp	.L6
+.L5:
+	movq	%rax, %rdx
+	shrq	%rdx
+	andl	$1, %eax
+	orq	%rax, %rdx
+	cvtsi2sdq	%rdx, %xmm0
+	addsd	%xmm0, %xmm0
+.L6:
+	movsd	%xmm0, -8(%rbp)
+	movsd	-8(%rbp), %xmm0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1495:
+	.size	_Z12elapsed_timev, .-_Z12elapsed_timev
+	.section	.rodata
+.LC2:
+	.string	" Total cycles: "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1496:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	-32(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	call	_Z11start_timerv
+	movsd	.LC0(%rip), %xmm0
+	movsd	%xmm0, -24(%rbp)
+	movl	$0, -28(%rbp)
+.L10:
+	movl	-32(%rbp), %eax
+	cmpl	%eax, -28(%rbp)
+	jge	.L9
+	movsd	-24(%rbp), %xmm1
+	movsd	.LC1(%rip), %xmm0
+	addsd	%xmm1, %xmm0
+	movsd	%xmm0, -24(%rbp)
+	addl	$1, -28(%rbp)
+	jmp	.L10
+.L9:
+	call	_Z12elapsed_timev
+	movq	%xmm0, %rax
+	movq	%rax, -16(%rbp)
+	leaq	.LC2(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-16(%rbp), %rax
+	movq	%rax, -40(%rbp)
+	movsd	-40(%rbp), %xmm0
+	movq	%rdx, %rdi
+	call	_ZNSolsEd@PLT
+	movq	%rax, %rdx
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rax
+	movq	%rax, %rsi
+	movq	%rdx, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L12
+	call	__stack_chk_fail@PLT
+.L12:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1496:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB1986:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L15
+	cmpl	$65535, -8(%rbp)
+	jne	.L15
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L15:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1986:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_start_time, @function
+_GLOBAL__sub_I_start_time:
+.LFB1987:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1987:
+	.size	_GLOBAL__sub_I_start_time, .-_GLOBAL__sub_I_start_time
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_start_time
+	.section	.rodata
+	.align 8
+.LC0:
+	.long	0
+	.long	1072693248
+	.align 8
+.LC1:
+	.long	0
+	.long	1073741824
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
