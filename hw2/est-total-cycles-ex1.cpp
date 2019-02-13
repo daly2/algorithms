94685d42308839317e9e3c40d493972d431c2723
Mike Daly <daly2@stolaf.edu>
2019-02-12 22:46:22 -0600

    assembly to c++ done

---
+++ hw2/est-total-cycles-ex1.cpp
@@ -0,0 +1,10 @@
+//31 clock
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x=3;
+    int y=5;
+    int z;
+    z=y-x;
+}
---
+++ hw2/est-total-cycles-ex2.cpp
@@ -0,0 +1,10 @@
+// cycles
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 0; i<=199; i+=2){
+        int x = 3;
+        x=x*4;
+    }
+}
