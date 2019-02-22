ab09ea7e3fbb4600e7a7151619438ff4ed81594f
Mike Daly <daly2@stolaf.edu>
2019-02-22 10:55:10 -0600

    switching to lab for timing

---
+++ hw6/timing-r.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include "elapsed_time.h"
+void recursion(int n)
+{
+  double x = 2 * 3;
+  x = x * 3;
+ 
+  if (n > 0) {
+    recursion(n/2);
+    recursion(n/3);
+  }
+}
+
+int main()
+{
+    int n = 100;
+    start_timer();
+    recursion(n);
+    double cycles = elapsed_time();
+    cout << "num cycles: " << cycles << "\n";
+    return 0;
+}
